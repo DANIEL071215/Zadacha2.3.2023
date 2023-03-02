a4af1184af7ed5d3d0f7c5aa7ba962b7c8074d72
DANIEL071215 <dakatadr@abv.bg>
2023-03-02 11:34:19 +0200

    Commit

---
+++ ZadachaVChas/Program.cs
@@ -0,0 +1,61 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadachaVChas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            List<int> numbers = Console.ReadLine().Split( ).Select(int.Parse).ToList();// chislata koito sme vuveli ot klaviaturata se printirat s space
+            PrintCount(numbers);
+
+            Console.WriteLine("");
+            int[] array1=Console.ReadLine().Split( ).Select(int.Parse).ToArray();// dobavqme nov masiv array1 i ni go chete 
+            AddArrayToList(numbers, array1);
+
+            Console.WriteLine("");
+            int number = int.Parse(Console.ReadLine()); 
+            IsNumberInList(numbers, number);
+
+            Console.WriteLine("");
+            int[] array2 = Console.ReadLine().Split( ).Select(int.Parse).ToArray();//dobavqme vtoriq masiv array2 i ni go chete
+            AddArrayToList2(numbers, array2);
+        }
+
+        private static void AddArrayToList2(List<int> numbers, int[] array2)
+        {
+          numbers.InsertRange(3,array2);
+            numbers.Sort();
+            Console.WriteLine($" Dobavqme vtoriq masiv: {string.Join("*" ,numbers)}");//tuk dobavqme vtoriq masiv i razdelqme chislata s *
+        }
+
+        private static void IsNumberInList(List<int> numbers, int number)
+        {
+            if (numbers.Contains(number))
+            {
+                Console.WriteLine($"Chisloto {number} e na {numbers.IndexOf(number)}");
+            }
+            else
+            {
+                Console.WriteLine($"Chisloto {number} ne e v spisuka");
+            }
+        }
+
+        private static void AddArrayToList(List<int> numbers, int[] array1)
+        {
+            numbers.AddRange(array1);
+            numbers.Sort();
+            Console.WriteLine($"Dobavqme purviq masiv: {string.Join(" " , numbers)}");
+        }
+
+        private static void PrintCount(List<int> numbers)
+        {
+            Console.WriteLine($"Broq na elementite na spisuka sa: {numbers.Count}");
+        }
+    }
+}
---
+++ zadacha 2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadacha_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            byte count = 1;
+            byte maxCount = 0;
+            int repeatedElements = 0;
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == numbers[i+1])
+                {
+                    count++;
+                    repeatedElements = numbers[i];  
+                }
+                else
+                {
+                    if (count>maxCount)
+                    {
+                        maxCount=count;
+                    }
+                    count = 1;
+                }
+            }
+        }
+    }
+}
